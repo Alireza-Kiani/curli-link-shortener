790bc140bd439404484f43f979cf2e0ca05314f8
raidenShadow <alirezakiaei@gmail.com>
2020-10-16 15:42:46 +0330

    refactor for controller with new services

--- src/controller/api.ts
+++ src/controller/api.ts
@@ -1,10 +1,10 @@
-import { RequestHandler } from 'express';
-import CRS from 'crypto-random-string';
+import { raw, RequestHandler } from 'express';
+// import CRS from 'crypto-random-string';
 import validator from 'validator';
 import Error from '../utils/error-handler';
-import ApiService from './service';
-import {RedisData} from "../@types/redis";
-import fetch from "node-fetch"
+// import ApiService from './service';
+// import { RedisData } from '../@types/redis';
+import fetch from 'node-fetch'
 
 const { API_VERSION } = process.env;
 
@@ -16,9 +16,18 @@ class ApiController {
             if (!validator.isURL(link)) {
                 throw new Error('Please provide a valid URL');
             }
-            const randomUniqueLink = CRS({ length: 5 });
-            await ApiService.setValue(API_VERSION!, randomUniqueLink, {link, date: new Date()});
-            return res.status(200).send({ shortLink: randomUniqueLink });
+            const raw_res_save = await fetch('http://curli.ir:8083/set', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    link
+                }),
+                redirect: 'follow'
+            });
+            const parsed_res = await raw_res_save.json();
+            return res.status(200).send(parsed_res);
         } catch (error) {
             return res.status(400).send(error);
         }        
@@ -27,20 +36,22 @@ class ApiController {
     redirectLink: RequestHandler = async (req, res, next) => {
         try {
             const { url } = req.params;
-            let foundLink = null;
-            const redisResponse: RedisData = await ApiService.getValue(API_VERSION!, url);
-            if (!redisResponse) {
-                const response = await fetch(`http://curli.ir:8081/${url}`);
-                const link = await response.json();
-                if (!link.originalLink) {
-                    throw new Error('Couldn\'t find any URL under the provided link');
-                } else {
-                    foundLink = link.originalLink
-                }
+            let foundLink: null | string = null;
+            // New strategy
+            const redis_raw_response = await fetch(`http://curli.ir:8083/${url}`);
+            if (redis_raw_response.status === 200) {
+                const parsed_redis_response = await redis_raw_response.json();
+                foundLink = parsed_redis_response.originalLink;
             } else {
-                foundLink = redisResponse.link;
+                const alternative_raw_response = await fetch(`http://curli:8081/${url}`);
+                if (alternative_raw_response.status === 200) {
+                    const parsed_alternative_response = await alternative_raw_response.json();
+                    foundLink = parsed_alternative_response.originalLink;
+                } else {
+                    throw new Error('Couldn\'t find any URL under the provided link'); 
+                }
             }
-            if (!/http:\/\/|https:\/\//gi.test(foundLink)) {
+            if (!/http:\/\/|https:\/\//gi.test(foundLink!)) {
                 foundLink = `https://${foundLink}`;
             }
             return res.status(301).redirect(`${foundLink}`);
