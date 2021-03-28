6b8048072e8d3c9f1cf2557ed45ee64e1e5fd336
Mahdi Karimi <mhdi_kr@outlook.com>
2021-03-29 00:32:14 +0430

    added gulp, postcss, changed some of service worker dirs.

--- src/public/js/functions.js
+++ src/public/js/functions.js
@@ -1,22 +1,12 @@
 async function registerSW() {
-
-
     if ('serviceWorker' in navigator) {
-
-
         try {
             await navigator.serviceWorker.register('/service-worker.js');
-
-
         } catch (e) {
             alert('ServiceWorker registration failed. Sorry about that.');
-
-
         }
     } else {
         document.querySelector('.alert').removeAttribute('hidden');
-
-
     }
 }
 window.addEventListener('load', (e) => {
--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -8,31 +8,16 @@ const staticAssets = [
     './images/favicon.png',
     './images/tail-spin.svg',
     './404.html',
-    './style/clouds.css',
-    './style/style.css',
-    './style/toggle.css',
-    './style/night.css'
+    './dist/curli.min.css'
 ];
-
 async function cacheFirst(req) {
     const cache = await caches.open(cacheName);
-
-
     const cachedResponse = await cache.match(req);
-
-
     return cachedResponse || fetch(req);
-
 }
-
 self.addEventListener('install', async event => {
-
     const cache = await caches.open(cacheName);
-
-
     await cache.addAll(staticAssets);
-
-
 });
 
 self.addEventListener('fetch', event => {
